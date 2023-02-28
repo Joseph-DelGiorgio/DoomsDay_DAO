3b6fb66cfce844c0701887f5068093b2f0ae9c45
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-02-27 19:50:12 -0700

    Update DDDao.sol

--- DoomDAO/contracts/DDDao.sol
+++ DoomDAO/contracts/DDDao.sol
@@ -69,3 +69,99 @@ contract DoomsdayDAO is ERC721, Ownable {
         daoWallet.transfer(balance);
     }
 }
+
+
+//Version 2 below (added in governance)
+
+/*
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract DoomsdayDAO is ERC721, Ownable {
+using Counters for Counters.Counter;
+
+
+// Define the key roles
+enum KeyRole {
+    EmergencyServices,
+    SearchAndRescue,
+    EngineeringAndConstruction,
+    LogisticsAndSupplyChainManagement,
+    CommunicationAndInformationTechnology,
+    SecurityAndLawEnforcement,
+    PsychologicalSupportAndCounseling,
+    SocialAndCommunityServices
+}
+
+// Define the NFT structure
+struct NFT {
+    uint256 id;
+    address owner;
+    KeyRole keyRole;
+}
+
+// Mapping to store NFTs
+mapping (uint256 => NFT) public nfts;
+uint256 public totalSupply;
+uint256 public constant MAX_NFTS = 100;
+uint256 public constant MIN_MINTING_PRICE = 0.1 ether;
+uint256 public constant MAX_MINTING_PRICE = 1 ether;
+uint256 public mintingPrice = 0.1 ether;
+address payable public daoWallet;
+
+// Events
+event NFTMinted(address owner, uint256 tokenId, KeyRole keyRole);
+event MintingPriceChanged(uint256 newPrice);
+event DaoWalletChanged(address newWallet);
+event DaoFunded(uint256 amount);
+
+// Modifiers
+modifier onlyDAO() {
+    require(msg.sender == daoWallet, "Only DAO can execute this function");
+    _;
+}
+
+// Constructor to set name and symbol
+constructor() ERC721("DoomsdayDAO", "DDAO") {
+    daoWallet = payable(msg.sender);
+}
+
+function mintNFT(address to, KeyRole keyRole) external payable {
+    require(totalSupply < MAX_NFTS, "Maximum NFT limit reached");
+    require(msg.value >= mintingPrice, "Insufficient payment");
+    uint256 tokenId = totalSupply + 1;
+    _safeMint(to, tokenId);
+    nfts[tokenId] = NFT(tokenId, to, keyRole);
+    totalSupply += 1;
+    emit NFTMinted(to, tokenId, keyRole);
+    daoWallet.transfer(msg.value);
+}
+
+function setMintingPrice(uint256 price) external onlyDAO {
+    require(price >= MIN_MINTING_PRICE && price <= MAX_MINTING_PRICE, "Invalid price");
+    mintingPrice = price;
+    emit MintingPriceChanged(price);
+}
+
+function setDaoWallet(address payable wallet) external onlyDAO {
+    daoWallet = wallet;
+    emit DaoWalletChanged(wallet);
+}
+
+function fundDao() external payable onlyDAO {
+    emit DaoFunded(msg.value);
+}
+
+function withdraw() external onlyDAO {
+    uint256 balance = address(this).balance;
+    daoWallet.transfer(balance);
+}
+
+}
+
+*/
