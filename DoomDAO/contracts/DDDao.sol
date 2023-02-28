6f172e3911c58d384bc2a3deb686b5368a7476b7
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-02-27 20:30:09 -0700

    Update DDDao.sol

--- DoomDAO/contracts/DDDao.sol
+++ DoomDAO/contracts/DDDao.sol
@@ -70,10 +70,7 @@ contract DoomsdayDAO is ERC721, Ownable {
     }
 }
 
-
-//Version 2 below (added in governance)
-
-/*
+//Version 2
 
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
@@ -83,85 +80,115 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract DoomsdayDAO is ERC721, Ownable {
-using Counters for Counters.Counter;
+    using Counters for Counters.Counter;
 
+    // Define the key roles
+    enum KeyRole {
+        EmergencyServices,
+        SearchAndRescue,
+        EngineeringAndConstruction,
+        LogisticsAndSupplyChainManagement,
+        CommunicationAndInformationTechnology,
+        SecurityAndLawEnforcement,
+        PsychologicalSupportAndCounseling,
+        SocialAndCommunityServices
+    }
 
-// Define the key roles
-enum KeyRole {
-    EmergencyServices,
-    SearchAndRescue,
-    EngineeringAndConstruction,
-    LogisticsAndSupplyChainManagement,
-    CommunicationAndInformationTechnology,
-    SecurityAndLawEnforcement,
-    PsychologicalSupportAndCounseling,
-    SocialAndCommunityServices
-}
+    // Define the NFT structure
+    struct NFT {
+        uint256 id;
+        address owner;
+        KeyRole keyRole;
+    }
 
-// Define the NFT structure
-struct NFT {
+    // Mapping to store NFTs
+    mapping (uint256 => NFT) public nfts;
+    uint256 public totalSupply;
+    uint256 public constant MAX_NFTS = 100;
+    uint256 public constant MIN_MINTING_PRICE = 0.1 ether;
+    uint256 public constant MAX_MINTING_PRICE = 1 ether;
+    uint256 public mintingPrice = 0.1 ether;
+    address payable public daoWallet;
+
+    // Event to track minting of NFTs
+    event NFTMinted(address owner, uint256 tokenId, KeyRole keyRole);
+
+    // Constructor to set name and symbol
+    constructor() ERC721("DoomsdayDAO", "DDAO") {
+        daoWallet = payable(msg.sender);
+    }
+
+    function mintNFT(address to, KeyRole keyRole) external payable {
+        require(totalSupply < MAX_NFTS, "Maximum NFT limit reached");
+        require(msg.value >= mintingPrice, "Insufficient payment");
+        uint256 tokenId = totalSupply + 1;
+        _safeMint(to, tokenId);
+        nfts[tokenId] = NFT(tokenId, to, keyRole);
+        totalSupply += 1;
+        emit NFTMinted(to, tokenId, keyRole);
+        daoWallet.transfer(msg.value);
+    }
+
+    function setMintingPrice(uint256 price) external onlyOwner {
+        require(price >= MIN_MINTING_PRICE && price <= MAX_MINTING_PRICE, "Invalid price");
+        mintingPrice = price;
+    }
+
+    function setDaoWallet(address payable wallet) external onlyOwner {
+        daoWallet = wallet;
+    }
+
+    function withdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+        daoWallet.transfer(balance);
+    }
+
+    struct Proposal {
     uint256 id;
-    address owner;
-    KeyRole keyRole;
-}
+    string description;
+    uint256 forVotes;
+    uint256 againstVotes;
+    bool executed;
+    mapping(address => bool) voted;
+    }
 
-// Mapping to store NFTs
-mapping (uint256 => NFT) public nfts;
-uint256 public totalSupply;
-uint256 public constant MAX_NFTS = 100;
-uint256 public constant MIN_MINTING_PRICE = 0.1 ether;
-uint256 public constant MAX_MINTING_PRICE = 1 ether;
-uint256 public mintingPrice = 0.1 ether;
-address payable public daoWallet;
+    mapping(uint256 => Proposal) public proposals;
+    uint256 public proposalCount;
 
-// Events
-event NFTMinted(address owner, uint256 tokenId, KeyRole keyRole);
-event MintingPriceChanged(uint256 newPrice);
-event DaoWalletChanged(address newWallet);
-event DaoFunded(uint256 amount);
+    function createProposal(string memory description) external {
+    proposalCount += 1;
+    Proposal storage newProposal = proposals[proposalCount];
+    newProposal.id = proposalCount;
+    newProposal.description = description;
+    newProposal.forVotes = 0;
+    newProposal.againstVotes = 0;
+    newProposal.executed = false;
+    }
 
-// Modifiers
-modifier onlyDAO() {
-    require(msg.sender == daoWallet, "Only DAO can execute this function");
-    _;
-}
 
-// Constructor to set name and symbol
-constructor() ERC721("DoomsdayDAO", "DDAO") {
-    daoWallet = payable(msg.sender);
-}
+    function voteForProposal(uint256 proposalId) external {
+        Proposal storage proposal = proposals[proposalId];
+        require(proposal.id != 0, "Proposal does not exist");
+        require(!proposal.voted[msg.sender], "Already voted");
+        proposal.forVotes += 1;
+        proposal.voted[msg.sender] = true;
+    }
 
-function mintNFT(address to, KeyRole keyRole) external payable {
-    require(totalSupply < MAX_NFTS, "Maximum NFT limit reached");
-    require(msg.value >= mintingPrice, "Insufficient payment");
-    uint256 tokenId = totalSupply + 1;
-    _safeMint(to, tokenId);
-    nfts[tokenId] = NFT(tokenId, to, keyRole);
-    totalSupply += 1;
-    emit NFTMinted(to, tokenId, keyRole);
-    daoWallet.transfer(msg.value);
-}
+    function voteAgainstProposal(uint256 proposalId) external {
+        Proposal storage proposal = proposals[proposalId];
+        require(proposal.id != 0, "Proposal does not exist");
+        require(!proposal.voted[msg.sender], "Already voted");
+        proposal.againstVotes += 1;
+        proposal.voted[msg.sender] = true;
+    }
 
-function setMintingPrice(uint256 price) external onlyDAO {
-    require(price >= MIN_MINTING_PRICE && price <= MAX_MINTING_PRICE, "Invalid price");
-    mintingPrice = price;
-    emit MintingPriceChanged(price);
-}
-
-function setDaoWallet(address payable wallet) external onlyDAO {
-    daoWallet = wallet;
-    emit DaoWalletChanged(wallet);
-}
-
-function fundDao() external payable onlyDAO {
-    emit DaoFunded(msg.value);
-}
-
-function withdraw() external onlyDAO {
-    uint256 balance = address(this).balance;
-    daoWallet.transfer(balance);
-}
+    function executeProposal(uint256 proposalId) external onlyOwner {
+    Proposal storage proposal = proposals[proposalId];
+    require(proposal.id != 0, "Proposal does not exist");
+    require(!proposal.executed, "Proposal already executed");
+    require(proposal.forVotes > proposal.againstVotes, "Not enough votes");
+    proposal.executed = true;
+    //Execute the proposal
+    }
 
 }
-
-*/
